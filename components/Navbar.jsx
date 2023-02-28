b9a6aafd52d4baf7bbf596c4d9d06f947db07931
Ayush Garg <humbleayush30@gmail.com>
2023-02-28 20:16:12 +0530

    fixed bug

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
             <Link href="/">
               <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
             </Link>
-            <Link href="/">
+            <Link href="/#about">
               <li className="ml-10 text-sm uppercase hover:border-b">About</li>
             </Link>
             <Link href="/#skills">
